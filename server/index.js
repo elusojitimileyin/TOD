d454942d640828bd2f7b1b029bd69905555a062b
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-11-11 09:40:17 +0100

    setup for server and database

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const app = express();
+// use express.json() to get data in JSON format
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 3000;
+
+// connect to MongoDB
+mongoose.connect(process.env.DB_CONNECT)
+.then(()=> console.log('Connect to database'))
+.catch(err => console.log('Error connecting to'))
+
+
+// add port and connect to server
+app.listen(PORT, ()=> console.log('server connected') );
